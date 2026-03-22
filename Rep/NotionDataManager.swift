e191286b8544abd281794eb659d214aea4f520e1
Alex Haidar <alex.haidar@microhealthllc.com>
2026-03-22 00:42:41 -0400

    NotionDataManger consolidated header and block import

--- Rep/NotionDataManager.swift
+++ Rep/NotionDataManager.swift
@@ -8,35 +8,7 @@ import Foundation
 import SwiftData
 
 
-public struct NotionSearchRequest: Codable {
-    public let results: [result]
-    public let object: String?
-    
-    public struct result: Codable {
-        public let id: String?
-        public let object: String?
-        public let properties: properties?
-        public let icon: Icon?
-        public let last_edited_time: Date?
-        public let created_time: Date?
-    }
-    
-    public struct Icon: Codable {
-        public let type: String?
-        public let emoji: String?
-    }
-    
-    public struct properties: Codable {
-        public let title: TitleDict?
-    }
-    
-    public struct TitleDict: Codable {
-        public let title: [TitleItem]
-    }
-    public struct TitleItem: Codable {
-        public let plain_text: String?
-    }
-}
+//TODO: move this into the cache file
 
 @Model final class NotionPageMetaData {                 ///Notion page metadata schema for on-demand syncing/retrieval feature
     @Attribute(.unique) public var pageID: String
@@ -57,18 +29,26 @@ public struct NotionSearchRequest: Codable {
     }
 }
 
-
-class NotionDataManager: ObservableObject {
-    static let shared = NotionDataManager()
+@MainActor
+public class NotionDataManager: ObservableObject {
+    static let shared: NotionDataManager = NotionDataManager(id: "", passEndpoint: "", userPageId: "")
     
     @Published var plain_text: String?
     @Published var id: String
+    
+    init(id: String, passEndpoint: String, userPageId: String) {
+        self.id = id
+        self.passEndpoint = passEndpoint
+        self.userPageId = userPageId
+    }
     
     enum ErrorDesc: LocalizedError {        ///start using this for logging local errors
         case authTokenError
         case urlRequestError
         case parsingError
         case encodeError
+        case decodeError
+        case paginationError
     }
     
     @MainActor
@@ -80,28 +60,25 @@ class NotionDataManager: ObservableObject {
         return token
     }
     
-    let searchEndpoint: URL = URL(string: "https://api.notion.com/v1/search")
-    private init(id: String) {
-        self.id = id
-    }
+    let searchEndpoint: URL = URL(string: "https://api.notion.com/v1/search")!
     
-    
-    private func getHeaders(token: String) async throws {
-        guard !token.isEmpty else { throw ErrorDesc.authTokenError }
-        
-        let passToken = try fetchAuthToken(context: token)
+    public func getHeaders(context: ModelContext) async throws {
+        let passToken = try fetchAuthToken(context: context)
         var urlRequest = URLRequest(url: searchEndpoint)
         
-        guard let req = urlRequest as URLRequest else { throw ErrorDesc.urlRequestError }
-        req.addValue("Bearer \(passToken)", forHTTPHeaderField: "Authorization")
-        req.addValue("2026-03-11", forHTTPHeaderField: "Notion-Version")
+        guard !passToken.isEmpty else { throw ErrorDesc.authTokenError }
+        
+        urlRequest.addValue("Bearer \(passToken)", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("2026-03-11", forHTTPHeaderField: "Notion-Version")
         
         do {
-            req.httpMethod = "POST"
-            let (data, response) = try await URLSession.shared.data(for: req)
+            urlRequest.httpMethod = "POST"
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
             guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { throw URLError(.badServerResponse) }
             
             guard let encodeData = String(data: data, encoding: .utf8) else { throw ErrorDesc.encodeError }
+            print("data encoded: \(encodeData)")
+            
             let decodePageData = JSONDecoder()
             decodePageData.dateDecodingStrategy = .custom { decoder in
                 
@@ -118,16 +95,127 @@ class NotionDataManager: ObservableObject {
                 
                 throw DecodingError.typeMismatch(Date.self, DecodingError.Context(codingPath: c.codingPath,
                                                                                   debugDescription: "Date string does not match expected format"))
-                let decodePage = try decoderPageData.decode(NotionSearchRequest.self, from: data)
             }
             
+            let decodePage = try decodePageData.decode(NotionSearchRequest.self, from: data)
             for i in decodePage.results {
-                print("RESULTS: \(i)")
+                print("Header results ✅: \(i)")
             }
-            
             
         } catch {
-            print("parsing error: \(ErrorDesc.parsingError)")
+            print("parsing error ❗️: \(ErrorDesc.parsingError)")
+        }
+    }
+    
+    var passEndpoint: String
+    var userContentPage: String?
+    var userPageId: String
+    var storePageIDSets: Set<String> = []
+    var constructedEndpoint: String = ""
+    var paginatedRequest: URL?
+    
+    @Published var mainBlockBody: [MainBlockBody.Block] = []
+    public func getBlocks(pageID: String, context: ModelContext) async throws {    ///import acc user's notion page
+        
+        let pageID: String = pageID
+        let pagesEndpoint: String = "https://api.notion.com/v1/blocks/" + "\(pageID)/children"
+        constructedEndpoint = pagesEndpoint
+        
+        guard let stringToUrl: URL = URL(string: constructedEndpoint) else { return }
+        var request: URLRequest = URLRequest(url: stringToUrl)
+        
+        let auth = try fetchAuthToken(context: context)
+        guard !auth.isEmpty else { throw ErrorDesc.authTokenError }
+        
+        request.addValue("2022-06-28", forHTTPHeaderField: "Notion-Version")
+        request.addValue("Bearer \(auth)", forHTTPHeaderField: "Authorization")
+        
+        do {
+            request.httpMethod = "GET"
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { throw ErrorDesc.urlRequestError }
+            
+            guard let encodeString: String = String(data: data, encoding: .utf8) else { throw ErrorDesc.encodeError }
+            
+            let decoder = JSONDecoder()
+            let decodeBlocks: MainBlockBody = try decoder.decode(MainBlockBody.self, from: data)
+            
+            var returnedResults: [MainBlockBody.Block] = decodeBlocks.results
+            var paginatedResults: Bool = decodeBlocks.has_more
+            var cursor = decodeBlocks.next_cursor
+            
+            while paginatedResults, let nextCursor: String = cursor {
+                if let next: String = decodeBlocks.next_cursor {
+                    
+                    let paginate: String = pagesEndpoint + "?page_size=100&start_cursor=\(cursor ?? "")"
+                    guard let paginateStringToUrl: URL = URL(string: paginate) else { return }
+                    
+                    paginatedRequest = paginateStringToUrl
+                    var paginationRequest: URLRequest = URLRequest(url: paginateStringToUrl)
+                    paginationRequest.addValue("2022-06-28", forHTTPHeaderField: "Notion-Version")
+                    paginationRequest.addValue("Bearer \(auth)", forHTTPHeaderField: "Authorization")
+                    
+                    let (paginatedData, _) = try await URLSession.shared.data(for: paginationRequest)
+                    let decodePaginatedData = try JSONDecoder().decode(MainBlockBody.self, from: paginatedData)
+                    
+                    returnedResults.append(contentsOf: decodePaginatedData.results)
+                    paginatedResults = decodePaginatedData.has_more
+                    cursor = decodePaginatedData.next_cursor
+                    
+                    print("paginated successfully ✅")
+                } else {
+                    print("pagination error ⚠️:", ErrorDesc.paginationError)
+                }
+                
+                var returnDecodedResults = returnedResults
+                for i in 0..<returnDecodedResults.count {
+                    var extractedFields: [String] = []
+                    let blockList = returnDecodedResults[i]
+                    
+                    switch blockList.type {
+                    case "numbered_list_item":
+                        if let n = blockList.numbered_list_item?.rich_text {
+                            extractedFields.append(MainBlockBody.joinContent(n))
+                        }
+                    case "bulleted_list_item":
+                        if let b = blockList.bulleted_list_item?.rich_text {
+                            extractedFields.append(MainBlockBody.joinContent(b))
+                        }
+                    case "heading_1":
+                        if let h1 = blockList.heading_1?.rich_text {
+                            extractedFields.append(MainBlockBody.joinContent(h1))
+                        }
+                    case "heading_2":
+                        if let h2 = blockList.heading_2?.rich_text {
+                            extractedFields.append(MainBlockBody.joinContent(h2))
+                        }
+                    case "heading_3":
+                        if let h3 = blockList.heading_3?.rich_text {
+                            extractedFields.append(MainBlockBody.joinContent(h3))
+                        }
+                    default: break
+                    }
+                    
+                    if let paragraph = returnDecodedResults[i].paragraph?.rich_text {
+                        
+                        let joinedContent: String = MainBlockBody.joinContent(paragraph)
+                        extractedFields.append(contentsOf: [joinedContent])
+                        print("ALL LISTS: \(joinedContent)")
+                    }
+                    returnDecodedResults[i].ExtractedFields = extractedFields
+                }
+                
+                await MainActor.run {
+                    self.mainBlockBody = returnDecodedResults
+                }
+                
+                let formattedString: String = returnDecodedResults.flatMap{ $0.ExtractedFields }.joined(separator: "\n").trimmingCharacters(in: .whitespacesAndNewlines)
+                let chunkedRows: [String] = formattedString.components(separatedBy: "\n• ").flatMap {$0.components(separatedBy: "\n")}
+                print("formatted & trimmed string: \(chunkedRows)")
+            }
+            
+        } catch {
+            print("error returning page blocks ❗️", ErrorDesc.parsingError)
         }
     }
 }
---
+++ Rep/NotionDataStructs.swift
@@ -0,0 +1,8 @@
+//
+//  NotionDataStructs.swift
+//  Rep
+//
+//  Created by alex haidar on 3/21/26.
+//
+
+import Foundation
