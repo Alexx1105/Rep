73563e7b0dc268099b8ba2c94fcf4c6f5209479d
Alex Haidar <alexh2877@gmail.com>
2026-08-11 23:04:17 -0400

    segmented UI picker for displaying notes and transcript per tab, UI enhancements

--- Rep/UIComponents.swift
+++ Rep/UIComponents.swift
@@ -1718,6 +1718,29 @@ struct MacHelperDirections: View {
 }
 
 
+struct TranscriptionSummaryPicker: View {
+    @Binding var transcriptTab: TrancriptTab
+    
+    enum TrancriptTab: String {
+        case notes
+        case transcript
+    }
+    
+    var body: some View {
+        ZStack {
+            Capsule().frame(width: 200, height: 30).foregroundStyle(Color.mmBackground)
+            
+            Picker("", selection: $transcriptTab) {
+                Text("Notes").tag(TrancriptTab.notes)
+                Text("Transcript").tag(TrancriptTab.transcript)
+                
+            }.pickerStyle(.segmented)
+                .frame(width: 200)
+        }
+    }
+}
+
+
 #Preview {
     PaymentMenuCard(isPresented:  .constant(true))
 }
@@ -1751,4 +1774,8 @@ struct MacHelperDirections: View {
 
 #Preview {
     MacHelperDirections()
+}
+
+#Preview {
+    TranscriptionSummaryPicker(transcriptTab: .constant(.notes))
 }
